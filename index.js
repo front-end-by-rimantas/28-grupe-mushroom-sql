cc0f0030d470956fa92e4e9db9bd3cd82b494b94
Rimantas <belauzas@yahoo.com>
2021-09-06 23:46:15 +0300

    Solution, parts 1-4

--- index.js
+++ index.js
@@ -7,14 +7,57 @@ app.init = async () => {
     const connection = await mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        database: 'your_database_name',
+        database: 'mushroom',
     });
 
     let sql = '';
     let rows = [];
+    let i = 0;
+
+    const upName = str => {
+        return str[0].toUpperCase() + str.slice(1);
+    }
 
     // LOGIC BELOW
 
+    // 1
+    sql = 'SELECT `mushroom`, `price` FROM `mushroom` ORDER BY `price` DESC';
+    [rows] = await connection.execute(sql);
+
+    console.log('Grybai:');
+    for (const { mushroom, price } of rows) {
+        console.log(`${++i}) ${upName(mushroom)} - ${price} EUR/kg`);
+    }
+
+    console.log('');
+    // 2
+    sql = 'SELECT `name` FROM `gatherer`';
+    [rows] = await connection.execute(sql);
+
+    const names = rows.map(obj => obj.name);
+    console.log(`Grybautojai: ${names.join(', ')}.`);
+
+    console.log('');
+    // 3
+    sql = 'SELECT `mushroom` \
+            FROM `mushroom` \
+            WHERE `price` = ( \
+                SELECT MAX(`price`) FROM `mushroom` \
+            )';
+    [rows] = await connection.execute(sql);
+
+    console.log(`Brangiausias grybas yra: ${upName(rows[0].mushroom)}.`);
+
+    console.log('');
+    // 4
+    sql = 'SELECT `mushroom` \
+            FROM `mushroom` \
+            WHERE `price` = ( \
+                SELECT MIN(`price`) FROM `mushroom` \
+            )';
+    [rows] = await connection.execute(sql);
+
+    console.log(`Pigiausias grybas yra: ${upName(rows[0].mushroom)}.`);
 
 }
 
